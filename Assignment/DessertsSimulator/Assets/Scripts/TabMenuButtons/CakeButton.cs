6efd508f0ab8925e2ad0c26efdded1b9cbb5bdae
peterkaki <210181053@stu.vtc.edu.hk>
2022-04-24 23:31:48 +0800

    Workshop Tab Panel

--- Assignment/DessertsSimulator/Assets/Scripts/TabMenuButtons/CakeButton.cs
+++ Assignment/DessertsSimulator/Assets/Scripts/TabMenuButtons/CakeButton.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CookieButton : MonoBehaviour
+public class CakeButton : MonoBehaviour
 {
     Text text;
+    public static bool cook = false;
 
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<Text>();
-        text.text = "You need to make a cookie now.";
+        text.text = "You need to make a cake now.";
+        cook = true;
     }
+
+   
 }
--- Assignment/DessertsSimulator/Assets/Scripts/TabMenuButtons/CookieButton.cs
+++ Assignment/DessertsSimulator/Assets/Scripts/TabMenuButtons/CookieButton.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CakeButton : MonoBehaviour
+public class CookieButton : MonoBehaviour
 {
     Text text;
+    public static bool cook = false;
 
     // Start is called before the first frame update
     void Start()
     {
         text = GetComponent<Text>();
-        text.text = "You need to make a cake now.";
+        text.text = "You need to make a cookie now.";
+        cook = true;
     }
+
 }
