812ecc38df4c7e4b47fcf3bdb5c5519613d24962
matthewyuhh <210116114@stu.vtc.edu.hk>
2022-04-25 17:28:58 +0800

    update ui

--- Assignment/Assets/Scripts/Workshop/TabButton.cs
+++ Assignment/Assets/Scripts/Workshop/TabButton.cs
@@ -7,12 +7,15 @@ public class TabButton : MonoBehaviour
     [SerializeField]
     private GameObject tabPanel;
 
+    [SerializeField]
+    private GameObject ButtonText;
+
     public static bool tab = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -24,12 +27,13 @@ public class TabButton : MonoBehaviour
             {
                 tab = false;
                 tabPanel.SetActive(false);
+                ButtonText.SetActive(true);
             }
             else
             {
                 tab = true;
                 tabPanel.SetActive(true);
-
+                ButtonText.SetActive(false);
             }
         }
     }
